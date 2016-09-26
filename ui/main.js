f1183ec14f8d1c6bccd9b939886dbe4cbae9bbd4
smeenakshi1997 <smeenakshi1997@gmail.com>
2016-09-26 21:58:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,17 @@ console.log('Loaded!');
 var element=document.getElementById('main text');
 
 element.innerHTML='New Value';
-var marginLeft=0;
+
+
+
  var img=document.getElementById('madi');
+ var marginLeft=0;
+ function moveRight(){
+     marginLeft=marginLeft+10;
+      img.style.marginLeft=marginLeft+'px';
+ }
  img.onclick=function()
 { 
-    marginLeft=marginLeft+5; 
     var interval=setInterval(moveRight,50);
-    img.style.marginLeft=marginLeft+'px';
+   
 };
