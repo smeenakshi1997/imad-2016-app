32ad8b57d5c44734cf69e469975c28fa54075dfc
smeenakshi1997 <smeenakshi1997@gmail.com>
2016-09-26 20:42:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,8 +60,6 @@ var htmlTemplate=`<html>
   <head>
   <title>
    ${title}
-   <meta name="viewport" content="width=device-width,initial scale=1"/>
-   <link href="/ui/style.css" rel="stylesheet" />
    </title>
    </head>
 <body>
