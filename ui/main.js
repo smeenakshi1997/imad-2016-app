136fe1de285fbea15e49321892f8b800e8064d64
smeenakshi1997 <smeenakshi1997@gmail.com>
2016-09-27 22:21:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,8 +12,8 @@ button.onclick=function(){
             if(request.status===200)
             {
                 var counter=request.responseText;
-               var span=document.getElementById('çount');
-              span.innerHTML=counter.toString();
+               var span=document.getElementById('count');
+               span.innerHTML=counter.toString();
             }
         }
     };
