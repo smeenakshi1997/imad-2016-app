a49283e366e89a8c3b72b937a10c66d3531fc6fe
smeenakshi1997 <smeenakshi1997@gmail.com>
2016-09-27 22:58:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,17 @@ button.onclick=function(){
     request.open('GET','http://smeenakshi1997.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+name[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
