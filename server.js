ede4e461b9f61a91bef5a0e24edce6ca571b1e72
smeenakshi1997 <smeenakshi1997@gmail.com>
2016-09-27 23:34:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,9 +111,9 @@ app.get('/:articleName', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
 
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
   res.send(JSON.stringify(names));
 });
