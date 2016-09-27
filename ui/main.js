84c19e3a4c82330da2e1bc7e0afe9c719c793f15
smeenakshi1997 <smeenakshi1997@gmail.com>
2016-09-27 23:52:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,16 +22,34 @@ button.onclick=function(){
     request.send(null);
 };
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
+     var request=new XMLHttpRequest();
     
-    var names=['name1','name2','name3','name4'];
+    request.onreadystatechange=function(){
+        
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var names=['name1','name2','name3','name4'];
+                var names=request.responseText;
+                names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+               
+            }
+        }
+    };
+        var nameInput=document.getElementById('name');
+        var name=nameInput.value;
+    
+    
+     request.open('GET','http://smeenakshi1997.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
